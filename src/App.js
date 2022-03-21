a57a08df1ab0ea5f750407013eef139cce8784ba
Jia Song <jiasong214@gmail.com>
2022-03-21 17:26:58 +1100

    fix: Fix router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Routers from './routers';
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <div className="App">
-      <Router basename='https://jiasong214.github.io/events-client/'>
+      <Router>
         <Header />
         <Routers />
         <Footer />
