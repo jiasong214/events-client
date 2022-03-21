11cba005003588bce292adaa0ed811d142a47403
Jia Song <jiasong214@gmail.com>
2022-03-21 20:15:08 +1100

    feat: Add wishlist number

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
+import { getUserInfo } from '../services/users';
 import '../style/header.scss';
 
 const Header = () => {
   const location = useLocation();
   const user = useSelector(state => state.user?.data);
   const [path, setPath] = useState("");
+  const [wishlist, setWishlist] = useState([])
 
   useEffect(() => {
     setPath(location.pathname);
+
+    if(user._id) {
+      getUserInfo(user._id)
+        .then((data) => setWishlist(data.wishlist))
+    }
   }, [location.pathname])
 
 
@@ -38,6 +45,14 @@ const Header = () => {
             className={path === '/wishlist' ? "active" : ""}
           >
             Wishlist
+            {
+              wishlist.length !== 0
+              ?
+              <span className='wishlist-num'>
+                {wishlist.length}
+              </span>
+              : ""
+            }
           </Link>
           :
           <Link
